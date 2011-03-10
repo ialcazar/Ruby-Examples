c1715f88b16ed6daf382d7f498cede3efeab83e9
ialcazar <israelalcazar@gmail.com>
2011-03-10 01:04:16 +0100

    Added Unit Test basic project

---
+++ unit_test/test/test_ejemplo_ruby.rb
@@ -0,0 +1,7 @@
+require  "test/unit"
+class TestEjemploRuby < Test::Unit::TestCase
+   def test_assert_false
+      assert_equal(3,3,"Vaya cagada tron")
+   end
+end
+
---
+++ unit_test/test/test_simple_number.rb
@@ -0,0 +1,10 @@
+File.expand_path(File.dirname(__FILE__) + "/../lib")
+
+require "simple_number"
+require "test/unit"
+class TestSimpleNumber < Test::Unit::TestCase
+  def test_simple
+	  assert_equal(4,SimpleNumber.new(2).add(2))
+	  assert_equal(6,SimpleNumber.new(2).multiply(3))
+  end
+end
